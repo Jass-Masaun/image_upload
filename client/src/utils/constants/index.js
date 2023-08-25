c9a44b7fd965492a463c0cd522decec8c74a4881
Jaspreet Singh <jaspreetsinghmasaun@gmail.com>
2023-08-25 18:47:53 +0530

    change domain

--- client/src/utils/constants/index.js
+++ client/src/utils/constants/index.js
@@ -1,5 +1,5 @@
 const ACCESS_TOKEN_KEY = "access_token";
-// const BASE_URL = "https://image-uplaod.onrender.com/api/v1";
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://image-uplaod.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:5000/api/v1";
 
 export { ACCESS_TOKEN_KEY, BASE_URL };
