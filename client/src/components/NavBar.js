169860debb428a415547b5d6b8f75f7c01c29e38
Jaspreet Singh <jaspreetsinghmasaun@gmail.com>
2023-08-26 14:15:32 +0530

    change package

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,35 +1,29 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import { ACCESS_TOKEN_KEY } from "../utils/constants";
 
 const NavBar = ({ buttons }) => {
   const handleLogout = () => {
     window.localStorage.removeItem(ACCESS_TOKEN_KEY);
-    window.location.href = "/login";
-  };
-
-  const handleDashboard = async () => {
-    window.location.href = "/dashboard";
   };
 
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <button
-          className="text-white text-xl font-bold"
-          onClick={handleDashboard}
-        >
+        <Link to="/dashboard" className="text-white text-xl font-bold">
           Dashboard
-        </button>
+        </Link>
         <div className="flex items-center space-x-4">
           {buttons.map((button) => (
             <button className="text-white" onClick={button.handleClick}>
               {button.name}
             </button>
           ))}
-          <button className="text-white" onClick={handleLogout}>
+          <Link to="/login" className="text-white" onClick={handleLogout}>
             Logout
-          </button>
+          </Link>
         </div>
       </div>
     </nav>
